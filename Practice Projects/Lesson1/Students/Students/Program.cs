0dd23bccc5cf2c58e7d8a54dbc517a4249cb1629
Barrett, Chantal <xxchantalbarrettxx@gmail.com>
2021-09-09 14:46:56 -0400

    Added Lesson 1's Practice Project 1.9

---
+++ Practice Projects/Lesson1/Students/Students/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace Students
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string file = @"C:\Users\barrec5\OneDrive - Dell Technologies\Documents\FSD_NET\FSD-NET\Practice Projects\Lesson1\Students\Students.txt";
+
+            // Print the data only if the file exists
+            if (System.IO.File.Exists(file))
+            {
+                string studentData = System.IO.File.ReadAllText(file);
+                Console.WriteLine(studentData);
+            }
+        }
+    }
+}
