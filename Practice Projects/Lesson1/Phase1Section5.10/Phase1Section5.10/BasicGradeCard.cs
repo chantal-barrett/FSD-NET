99c07f7df9a188b01019d193e16948e75cc56b1f
Barrett, Chantal <xxchantalbarrettxx@gmail.com>
2021-09-27 09:06:07 -0400

    Changes have been committed.

---
+++ Practice Projects/Lesson1/Phase1Section5.10/Phase1Section5.10/BasicGradeCard.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phase1Section5._10
+{
+    class BasicGradeCard
+    {
+        protected int totalMarks;
+
+        public BasicGradeCard()
+        {
+            totalMarks = 0;
+        }
+
+        public virtual int getTotalMarks()
+        {
+            return totalMarks;
+        }
+    }
+}
