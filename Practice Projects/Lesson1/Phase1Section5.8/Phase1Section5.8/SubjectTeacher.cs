3d2ea87063ee14c156cdba75b240f6b364c8d6b5
Barrett, Chantal <xxchantalbarrettxx@gmail.com>
2021-09-27 09:01:35 -0400

    Changes have been committed.

---
+++ Practice Projects/Lesson1/Phase1Section5.8/Phase1Section5.8/SubjectTeacher.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phase1Section5._8
+{
+    class SubjectTeacher: Teacher
+    {
+        private SubjectTeacher whichSubject;
+
+        public SubjectTeacher WhichSubject
+        {
+            get { return whichSubject; }
+            set { whichSubject = value; }
+        }
+    }
+}
